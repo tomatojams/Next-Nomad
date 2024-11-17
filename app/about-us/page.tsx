cef2c364698e8db65faa1fa12188f96dc548b651
tomatojams <tomatojams@naver.com>
2024-11-17 18:03:05 +0900

    first commit

---
+++ app/about-us/page.tsx
@@ -0,0 +1,9 @@
+import Navigation from "../../components/navigation";
+
+export default function page() {
+  return (
+    <>
+      <h1>About Us</h1>
+    </>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import Navigation from "../components/navigation";
+
+export const metadata :Metadata = {
+  title: {
+    default: "Next", // 페이지를 못찾을때도 default값이 표시된다.
+    template: "%s | Next",
+  },
+  description: "메타테이다 연습",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navigation />
+        {children}
+      </body>
+    </html>
+  );
+}
